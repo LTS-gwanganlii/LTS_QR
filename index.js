b0b992b78e725bbbd2b81fcbbc8a264cd3df8755
LTS-gwanganlii <lts.gwangalli@gmail.com>
2025-08-26 19:29:30 +0900

    우수사례 나쁜사례 예시 출력

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const userLanguage = navigator.language || navigator.languages[0];
   let request, instaRequest1, instaRequest2, hashtag;
+
   if (userLanguage === "ko-KR") {
     request = "사진 1장 리뷰 2줄 부탁드립니다!";
     instaRequest1 = "스토리로 작성해주시고 매장을 태그 해 주세요!";
@@ -8,7 +9,7 @@ window.onload = function () {
       "해시태그를 자동으로 복사하였습니다! 스토리 작성후 붙혀넣기 해주세요";
     hashtag =
       "#부산데이트코스 #lasertagsports #레이저태그스포츠 #레이저태그스포츠광안점 #부산서바이벌 #광안리데이트 #레이저서바이벌 #광안리놀거리 #광안리핫플 #부산핫플 #부산여행 #부산가족여행 #부산데이트 #광안리데이트코스 #부산여행코스 #부산맛집 #부산카페 #광안리맛집 #광안리카페 #광안리놀거리 #광안리놀만한곳";
-  } else if (userLanguage === "en-US") {
+  } else {
     request = "Please take a photo and write a 2-sentence review!";
     instaRequest1 = "Please write a story and tag the store!";
     instaRequest2 =
@@ -17,20 +18,33 @@ window.onload = function () {
       "#BusanTourism #BusanVacation #BusanAttractions #GwangalliBeach #Gwangalli #GwangalliDate #LaserTagBusan #LaserTagGwangalli #BusanNightlife #BusanPlayground #ThingsToDoInBusan #BusanActivities #BusanRestaurants #GwangalliRestaurants #BusanFamilyTravel #GwangalliCafe #GwangalliTravel #VisitGwangalli #BusanBeach #TravelToBusan #KoreaTravel #SouthKoreaTravel #ExploreBusan #KoreanVacation #BusanHoliday";
   }
 
-
-  
   const naver = document.querySelector(".naver");
   const google = document.querySelector(".google");
   const insta = document.querySelector(".insta");
+
+  function showExamplesAndRedirect(callback) {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "block";
+
+    setTimeout(() => {
+      modal.style.display = "none";
+      callback();
+    }, 3000);
+  }
+
   naver.addEventListener("click", () => {
     alert(request);
-    window.location.href =
-      "https://m.place.naver.com/place/1149712929/review/visitor?entry=pll&reviewSort=recent";
+    showExamplesAndRedirect(() => {
+      window.location.href =
+        "https://m.place.naver.com/place/1149712929/review/visitor?entry=pll&reviewSort=recent";
+    });
   });
 
   google.addEventListener("click", () => {
     alert(request);
-    window.location.href = "https://g.page/r/CXfAXMEY8nB9EAI/review";
+    showExamplesAndRedirect(() => {
+      window.location.href = "https://g.page/r/CXfAXMEY8nB9EAI/review";
+    });
   });
 
   insta.addEventListener("click", () => {
@@ -39,12 +53,13 @@ window.onload = function () {
     alert(instaRequest2);
 
     navigator.clipboard.writeText(hashtag);
-    if (/android/.test(navigator.userAgent.toLowerCase())) {
-      window.location.href =
-        "https://www.instagram.com/";
-      return;
-    } else {
-      window.location.href = "instagram://camera";
-    }
+
+    showExamplesAndRedirect(() => {
+      if (/android/.test(navigator.userAgent.toLowerCase())) {
+        window.location.href = "https://www.instagram.com/";
+      } else {
+        window.location.href = "instagram://camera";
+      }
+    });
   });
 };
